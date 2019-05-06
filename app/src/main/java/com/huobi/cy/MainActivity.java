5a51426c2685a3e34208f7a372daa0bc9365996a
陈琰 <chenyan@huobi.com>
2019-05-06 14:56:17 +0800

    commit for test read and write object list.

--- app/src/main/java/com/huobi/cy/MainActivity.java
+++ app/src/main/java/com/huobi/cy/MainActivity.java
@@ -11,6 +11,8 @@ import com.huobi.cy.protobuf.UserInfo;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,6 +28,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mainBinding.btnRead.setOnClickListener(this);
         mainBinding.btnWrite.setOnClickListener(this);
+        mainBinding.btnReadList.setOnClickListener(this);
+        mainBinding.btnWriteList.setOnClickListener(this);
     }
 
     @Override
@@ -36,7 +40,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btn_write:
                 bytesData = testWritePB(mainBinding.etAccount.getText().toString(), mainBinding.etPassword.getText().toString());
-                Log.d("MainActivity", "bytesData: " + bytesData.toString());
+                StringBuilder sb = new StringBuilder();
+                for(int i = 0;i < bytesData.length;i ++) {
+                    sb.append(String.format("%8s", Integer.toBinaryString(bytesData[i] & 0xFF)).replace(' ', '0')).append(" ");
+                }
+                Log.d("MainActivity", "bytesData: " + sb.toString());
+                break;
+            case R.id.btn_read_list:
+                testReadList();
+                break;
+            case R.id.btn_write_list:
+                bytesData = testWriteList(mainBinding.etAccount.getText().toString(), mainBinding.etPassword.getText().toString());
+                StringBuilder sb1 = new StringBuilder();
+                for(int i = 0;i < bytesData.length;i ++) {
+                    sb1.append(String.format("%8s", Integer.toBinaryString(bytesData[i] & 0xFF)).replace(' ', '0')).append(" ");
+                }
+                Log.d("MainActivity", "bytesData: " + sb1.toString());
                 break;
             default:
                 break;
@@ -74,5 +93,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } catch (IOException e) {
         }
         return baos.toByteArray();
+    }
+
+    /**
+     * 写list
+     * @return
+     */
+    public byte[] testWriteList(String account, String password) {
+        UserInfo.ListOfUsers.Builder builder = UserInfo.ListOfUsers.newBuilder();
+        for(int i = 0;i < 100;i ++) {
+            UserInfo.User.Builder user = UserInfo.User.newBuilder();
+            user.setAccount(account + i);
+            user.setPassword(password + i);
+            builder.addUsers(user);
+        }
+        //对象写入字节数组输出流
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            builder.build().writeTo(baos);
+        } catch (IOException e) {
+        }
+        return baos.toByteArray();
+    }
+
+    /**
+     * 读List
+     */
+    public void testReadList() {
+        if(bytesData != null) {
+            try {
+                //字节数组解析成实体对象
+                UserInfo.ListOfUsers listOfUsers = UserInfo.ListOfUsers.parseFrom(bytesData);
+                List<UserInfo.User> list = listOfUsers.getUsersList();
+                Iterator<UserInfo.User> iterator = list.iterator();
+                StringBuilder sb = new StringBuilder();
+                while (iterator.hasNext()) {
+                    UserInfo.User user = iterator.next();
+                    sb.append(user.getAccount()).append(",").append(user.getPassword()).append("\n");
+                }
+                mainBinding.tvProtobuf.setText(sb.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
